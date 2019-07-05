3d9fa7e224561c2ee7a926d6e17469f2c612ce63
Michael Nitze <michael.nitze@michael-nitze.de>
2019-07-05 20:56:16 +0200

    Fix TypeScript definitions for icns-encoder.js.

--- lib/icns-encoder.d.ts
+++ lib/icns-encoder.d.ts
@@ -3,10 +3,10 @@
  * @param channel Raw image channel (one of ARGB).
  * @returns Packbits compressed channel (compression with regard to Apple ICNS format).
  */
-export function encode(channel: Buffer): Buffer;
+export declare function encode(channel: Buffer): Buffer;
 /**
  * Uncompress a PackBits (Apple ICNS) compressed input channel.
  * @param channel Compressed input channel (one of ARGB).
  * @returns Uncompressed channel.
  */
-export function decode(buffer: Buffer): Buffer;
+export declare function decode(buffer: Buffer): Buffer;
